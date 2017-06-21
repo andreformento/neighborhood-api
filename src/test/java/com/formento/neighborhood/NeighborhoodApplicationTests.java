b857e922b8991d43dae842b5202f7703e9056e53
André Formento <andreformento.sc@gmail.com>
2017-06-20 22:50:45 -0300

    Test main

--- src/test/java/com/formento/neighborhood/NeighborhoodApplicationTests.java
+++ src/test/java/com/formento/neighborhood/NeighborhoodApplicationTests.java
@@ -9,8 +9,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class NeighborhoodApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void shouldCallMain() {
+        NeighborhoodApplication.main(new String[]{});
+    }
 
 }
