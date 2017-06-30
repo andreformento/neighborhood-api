47dfbbaebc3ef83f02e3c99e0dfcfbf25397dc3b
andre.formento <andre.formento@concrete.com.br>
2017-06-30 18:16:15 -0300

    Add swagger

--- src/main/java/com/formento/neighborhood/api/controller/PropertyController.java
+++ src/main/java/com/formento/neighborhood/api/controller/PropertyController.java
@@ -4,19 +4,16 @@ import com.formento.neighborhood.api.mapper.PropertiesMapper;
 import com.formento.neighborhood.model.Boundary;
 import com.formento.neighborhood.model.Property;
 import com.formento.neighborhood.service.PropertyService;
-import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/properties")
+@Validated
 public class PropertyController {
 
     private final PropertyService propertyService;
@@ -40,7 +37,7 @@ public class PropertyController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public PropertiesMapper findPropertiesInsideBoundary(Integer ax, Integer ay, Integer bx, Integer by) {
+    public PropertiesMapper findPropertiesInsideBoundary(@NotNull final Integer ax, @NotNull final Integer ay, @NotNull final Integer bx, @NotNull final Integer by) {
         return new PropertiesMapper(propertyService.findPropertiesInsideBoundary(new Boundary(ax, ay, bx, by)));
     }
 
